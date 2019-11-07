85365074cb128682aa523a04618bf3f2b1248240
Tathagat <tathagatswagverma@gmail.com>
2019-11-07 10:06:36 +0530

    bugfix in models.py

--- users/models.py
+++ users/models.py
@@ -1,24 +1,27 @@
 # users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 class CustomUser(AbstractUser):
     # add additional fields in here
     firstname =  models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     def __str__(self):
-        return self.email
+        return self.usenamername
 
 def contact_default():
-    return {"firstname": "Webmaster",
-            "lastname":"Sumanyu"}
+    return {
+        "firstname": "Webmaster",
+        "lastname":"Sumanyu"
+        }
 
 class Relationship(models.Model):
-	active_id = ForeignKey(CustomUser)
-	receiver_id = ForeignKey(CustomUser)
+	active_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+	receiver_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name="friend")
 	relationship_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	net_balance = models.IntegerField(default=0)
 
-class Transaction:
-	relationship = models.ForeignKey()
-	transaction_id = models.UUIDField(default=uuid.uuid4, editable=False)
+# class Transaction:
+# 	relationship = models.ForeignKey(on_delete=models.CASCADE)
+# 	transaction_id = models.UUIDField(default=uuid.uuid4, editable=False)

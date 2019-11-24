9cefa11ac0629d37ddbc45144608760221e1be3a
Samad Koita <53687543+samadkoita@users.noreply.github.com>
2019-11-24 19:44:50 +0530

    Revert "Samad"

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser,Transaction
+from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('username', 'email',)
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['email'].required = False
 
 class CustomUserChangeForm(UserChangeForm):
 
@@ -18,21 +17,12 @@ class CustomUserChangeForm(UserChangeForm):
         fields = ('firstname','lastname', 'email','avatar')
     def __init__(self, *args, **kwargs):
         super(CustomUserChangeForm, self).__init__(*args, **kwargs)
-        self.fields['email'].required = False
-        self.fields['firstname'].required = False
-        self.fields['lastname'].required = False
-        self.fields['avatar'].required = False
 
         
 
 class FriendForm(forms.Form):
-	email = forms.EmailField()
+    username = forms.CharField(max_length=150)
 
-class TransactionFriendForm(forms.Form):
-	trans_amt=forms.IntegerField(label="Add Transaction Amount:")
-	C=[('pay','pay'),('receive','receive')]
-	trans_choice=forms.ChoiceField(choices=C,widget=forms.RadioSelect)
-	trans_text=forms.CharField(label="Transaction Name:")
-	CHOICES=(('None','None'),('Work','Work'),('Personal','Personal'),('Other','Other'))
-	trans_tag=forms.ChoiceField(label='Add Transaction Tag:',choices=CHOICES,widget=forms.Select(),required=False)
-
+class GroupForm(forms.Form):
+    grp_name = forms.CharField(max_length=150)
+    number_friends = forms.IntegerField(initial=1)

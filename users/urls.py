87ed8e6e3c1a42461930c9cc6c049ca54f03b9fa
Samad Koita <53687543+samadkoita@users.noreply.github.com>
2019-11-25 11:02:39 +0530

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -9,12 +9,7 @@ urlpatterns = [
     path('update/',views.profileupdate,name = 'update'),
     path('friend/<id>/', views.FriendTabView.as_view(), name='friend'),
     path('friend/', views.SignUpView.as_view(), name='friend'),
-    path('group/<int:grp_id>/<int:id>/', views.GroupView.as_view(), name='group'),
     path('create_group/<int:id>/', views.CreateGroupView.as_view(), name='create_group'),
-    path('create_transaction/<int:grp_id>/'
-    , views.CreateTransactionView.as_view(), name='create_transaction'),
-    path('create_transaction/<int:grp_id>/<int:id>/'
-    , views.CreateTransactionView.as_view(), name='create_transaction'),
     path('friend/<id1>/<id2>/',views.RelationshipView.as_view(),name='relationship'),
     path('friend/<id1>/<id2>/settle/',views.settle_friend,name='settle_friend'),
 ]
